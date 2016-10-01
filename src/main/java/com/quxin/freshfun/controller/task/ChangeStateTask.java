00c379638aa0534c75402d3d98aecb88acb1bf10
Tu ZL <18721394619@163.com>
2016-10-02 01:40:00 +0800

    删除订单状态修改的定时任务，已经在后端写了定时器

--- src/main/java/com/quxin/freshfun/controller/task/ChangeStateTask.java
+++ src/main/java/com/quxin/freshfun/controller/task/ChangeStateTask.java
@@ -1,24 +1,24 @@
-package com.quxin.freshfun.controller.task;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Controller;
-
-import com.quxin.freshfun.service.order.OrderDetailsService;
-
-/**
- * 定时任务 发货后的七天之后默认收货
- * @author TuZl
- * @time 2016年9月20日上午11:26:01
- */
-@Controller
-public class ChangeStateTask {
-
-	@Autowired
-	private OrderDetailsService orderDetailsService;
-	
-	@Scheduled(cron="0 0 1 * * ? ")   //每天1点钟触发      
-	public void receivingStateTask(){   
-		orderDetailsService.updateOrderStatus3();
-	} 
-}
+//package com.quxin.freshfun.controller.task;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Controller;
+//
+//import com.quxin.freshfun.service.order.OrderDetailsService;
+//
+///**
+// * 定时任务 发货后的七天之后默认收货
+// * @author TuZl
+// * @time 2016年9月20日上午11:26:01
+// */
+//@Controller
+//public class ChangeStateTask {
+//
+//	@Autowired
+//	private OrderDetailsService orderDetailsService;
+//
+//	@Scheduled(cron="0 0 1 * * ? ")   //每天1点钟触发
+//	public void receivingStateTask(){
+//		orderDetailsService.updateOrderStatus3();
+//	}
+//}
